085d5bdbe4850177ca45bfd753d15e75eaf1743f
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-07 16:52:31 +0530

    Create 3 Total subarrays with atmost k sum.java

---
+++ 1 Sliding Window/2 Problems/3 Total subarrays with atmost k sum.java
@@ -0,0 +1,27 @@
+// Application of Framework 2 : 
+// Problem-3 : Number of subarrays with atmost k sum
+public class Solution
+{
+    public static int solve(int[] nums, int k) {
+        int tail = 0; int head = -1; long total_subarrays = 0; long sum = 0;
+        
+        while(tail < nums.length) {
+            while (head + 1 < nums.length && sum + nums[head + 1] <= k) {
+                head++;
+                sum += nums[head];
+            }
+            
+            total_subarrays += head - tail + 1;
+            
+            if(tail > head) {
+                tail++;
+                head = tail - 1;
+            }else {
+                sum -= nums[tail];
+                tail++;
+            }
+        }
+        return total_subarrays;
+    }
+  }
+}
