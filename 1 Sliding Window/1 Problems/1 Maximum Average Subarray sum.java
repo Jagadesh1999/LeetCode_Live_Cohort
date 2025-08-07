aafa36dbdf8c2aefea22cb9eaef9c69c4bc11e01
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-07 14:53:29 +0530

    Update and rename 1 Sliding Window/1 Frameworks.java to 1 Sliding Window/1 Problems/1 Maximum Average Subarray sum.java

---
+++ 1 Sliding Window/1 Problems/1 Maximum Average Subarray sum.java
@@ -0,0 +1,23 @@
+// Maximum Average Subarray Sum 
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int currSum = 0; int maxSum = Integer.MIN_VALUE; int left = 0; int right = 0;
+        while(right < nums.length) {
+            // Step 1 : Allow always
+            currSum += nums[right];
+            
+            // Step 2 : Can we remove..!?
+            if(right >= k) {
+                currSum -= nums[left];
+                left++;
+            }
+            
+            // Step 3 : Is it k-sized 
+            if(right - left + 1 == k) {
+                maxSum = Math.max(maxSum, currSum);
+            }
+            right++;
+        }
+        return (double) maxSum / k;
+    }
+} // TC : O(n)
