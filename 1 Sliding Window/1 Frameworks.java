45c3772aa8835d74d77d05b2289c4394f8e80a26
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-07 15:22:15 +0530

    Create 1 Frameworks.java

---
+++ 1 Sliding Window/1 Frameworks.java
@@ -0,0 +1,27 @@
+// Sliding window framework for fixed size variation.
+
+public class Solution
+{
+    public static int solve(int[] nums, int d) {
+        // Declare and initialise the necessary data structures and the variables
+        int left = 0; int right = 0;
+        
+        while(right < nums.length) {
+            // Step 1 : Always Allow
+            // Expand the right marker
+            
+            // Step 2 : Can we remove?
+            if(right >= d) {
+                // Shrink the left marker according to the problem statement
+                left++;
+            }
+            
+            // Step 3 : Update if the window is k-sized
+            if(right - left + 1 == d) {
+               // Update the ans
+            }
+            right++;
+        }
+        return // ans;
+    } 
+  } // TC : O(n)
