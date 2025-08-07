44c1428d7e49665e966a6f16d923857a7d0b34a4
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-07 15:45:59 +0530

    Update 1 Longest Subarray with atmost k flips.java

--- 1 Sliding Window/2 Problems/1 Longest Subarray with atmost k flips.java
+++ 1 Sliding Window/2 Problems/1 Longest Subarray with atmost k flips.java
@@ -1,1 +1,27 @@
+// 
+
+public class Solution
+{
+    public static void solve(int[] nums, int k) {
+        int tail = 0; int head = -1; int counter = 0; int sol = 0;
+        
+        while(tail < nums.length) {
+            while(head+1 < nums.length && (nums[head+1] == 1 || counter < k)) {
+                head++;
+                if(nums[head] == 0) counter++;
+            }
+            
+            sol = Math.max(sol, head - tail + 1);
+            
+            if(tail > head) {
+                tail ++;
+                head = tail - 1;
+            } else {
+                if(nums[tail] == 0) counter--;
+                tail++;
+            }
+        }
+        return sol;
+    }
+} // TC : O(n)
 
