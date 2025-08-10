dd037cea72255a76495c4c61089bc868db4f1564
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-10 20:03:21 +0530

    Create 1.4 Total subarrays with exactly k uniques.java

---
+++ 1 Sliding Window/2 Problems/1.4 Total subarrays with exactly k uniques.java
@@ -0,0 +1,53 @@
+// Application of Framework 2 : 
+// Problem-4 : No. of subarrays with exactly k distinct elements
+public class Solution
+{
+    public static void exactlyKDistinct(int[] nums, int k) {
+        System.out.println(atmostKDistinct(nums, k) -  atmostKDistinct(nums, k - 1));
+    }
+    
+    public static int atmostKDistinct(int[] nums, int k) {
+        int tail = 0; int head = -1;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        int countDistinct = 0;
+        
+        int total_subarrays = 0;
+        
+        while(tail < nums.length) {
+            while(head+1 < nums.length && (map.getOrDefault(nums[head+1], 0) >= 1 || countDistinct < k)) {
+                head++;
+                map.put(nums[head], map.getOrDefault(nums[head], 0) + 1);
+                if(map.get(nums[head]) == 1) countDistinct++;
+            }
+            
+            total_subarrays += head - tail + 1;
+            
+            if(tail > head) {
+                tail ++;
+                head = tail - 1;
+				map.clear();
+                distinctCount = 0;  // Reset state as window is empty
+            } else {
+                map.put(nums[tail], map.getOrDefault(nums[tail], 0) - 1);
+                if(map.get(nums[tail]) == 0) countDistinct--;
+                tail++;
+            }
+        }
+        return total_subarrays;
+    }
+    
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt();
+
+        while(testcases != 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++) nums[i] = sc.nextInt();
+            exactlyKDistinct(nums, k);
+            testcases--;
+        }
+    }
+}
