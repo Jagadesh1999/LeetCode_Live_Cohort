345e19e8029af6723850932d2ab31c2dc8594625
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-11 00:27:18 +0530

    Update 1 Maximum Subarray.java

--- 1 Sliding Window/3 Kadane's Algorithm - Problems/1 Maximum Subarray.java
+++ 1 Sliding Window/3 Kadane's Algorithm - Problems/1 Maximum Subarray.java
@@ -1,17 +1,16 @@
-Given an integer array nums, find the subarray with the largest sum, and return its sum.
-Example 1:
-
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: The subarray [4,-1,2,1] has the largest sum 6.
-Example 2:
-
-Input: nums = [1]
-Output: 1
-Explanation: The subarray [1] has the largest sum 1.
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+// Example 1:
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+// Example 2:
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
 
 /*
-The brute force approach will take us O(n^2) to pick each of the subarray one by one and compute the sum to find the max sum for this problem
+The brute force approach will take us O(n^2) to pick each of the subarray one by one and compute the sum to find the 
+max sum for this problem
 */
 
 // Kadane's Algorithm :
