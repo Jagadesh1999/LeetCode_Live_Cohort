afd7547d331239023d588cc94f134a9a3d02d918
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-10 20:21:36 +0530

    Create 1.7 Longest Substring with atleast K unqiues.java

---
+++ 1 Sliding Window/2 Problems/1.7 Longest Substring with atleast K unqiues.java
@@ -0,0 +1,35 @@
+// 7. Longest Substring with atleast k distinct
+
+class Solution {
+    public int longestKSubstr(String s, int k) {
+        int tail = 0; int head = -1;
+        
+        Map<Character, Integer> map = new HashMap<>();
+        int distinctCount = 0;
+        
+        int resMax = -1;
+        
+        while(tail < s.length()) {
+            while(head+1 < s.length() && (map.getOrDefault(s.charAt(head+1), 0) > 0)) {
+                head++;
+                map.put(s.charAt(head), map.getOrDefault(s.charAt(head), 0)+1);
+                if(map.get(s.charAt(head)) == 1) distinctCount++;
+            }
+            
+            if(distinctCount >= k) resMax = Math.max(resMax, head - tail + 1);
+            
+            if(tail > head) {
+                tail++;
+                head = tail - 1;
+                map.clear();
+            } else {
+                map.put(s.charAt(tail), map.get(s.charAt(tail))-1);
+                if(map.get(s.charAt(tail)) == 0) {
+                    distinctCount--;
+                }
+                tail++;
+            }
+        }
+        return resMax;
+    }
+} // TC : O(n)
