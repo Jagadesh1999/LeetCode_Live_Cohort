80a88bef210551b310380f6d058aa958aae9eb18
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-12 22:49:23 +0530

    Update 1.2 Can Place Flowers.java

--- 2 Prefix Sum/LeetCode Problems/1.2 Can Place Flowers.java
+++ 2 Prefix Sum/LeetCode Problems/1.2 Can Place Flowers.java
@@ -12,3 +12,60 @@ Example 1:
 Input: flowerbed = [1,0,0,0,1], n = 1
 Output: true
 */
+
+// class Solution {
+//     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+//         if(n == 0) return true;
+
+//         for(int i = 0; i < flowerbed.length; i++) {
+//             if(i == 0 && i == flowerbed.length - 1) {
+//                 if(flowerbed[i] == 0) {
+//                     flowerbed[i] = 1;
+//                     n--;
+//                     if(n == 0) return true;
+//                 }
+//             }else if(i == 0 && i != flowerbed.length - 1) {
+//                 if(flowerbed[i] == 0 && flowerbed[i+1] == 0) {
+//                     flowerbed[i] = 1;
+//                     n--;
+//                     if(n == 0) return true;
+//                 }
+//             } else if(i == flowerbed.length - 1) {
+//                 if(flowerbed[i] == 0 && flowerbed[i-1] == 0) {
+//                     flowerbed[i] = 1;
+//                     n--;
+//                     if(n == 0) return true;
+//                 }
+//             } else {
+//                 if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0) {
+//                     flowerbed[i] = 1;
+//                     n--;
+//                     if(n == 0) return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// } // TC : O(n)
+
+Classic and Elegant Implementation : 
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n == 0) return true;
+
+        for(int i = 0; i < flowerbed.length; i++) {
+            if(flowerbed[i] == 0) {
+                boolean prevIsEmpty = ((i == 0) || (flowerbed[i-1] == 0));
+                boolean nextIsEmpty = ((i == flowerbed.length-1) || (flowerbed[i+1] == 0));
+
+                if(prevIsEmpty && nextIsEmpty) {
+                    flowerbed[i] = 1;
+                    n--;
+                    if(n == 0) return true;
+                }
+            }
+        }
+        return false;
+    }
+} // TC : O(n)
