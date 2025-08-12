22f70e47d5b5d1b299ff0c559c4ce21aa9a6869a
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-13 02:06:17 +0530

    Create 1.4 Contiguous Array.java

---
+++ 2 Prefix Sum/LeetCode Problems/1.4 Contiguous Array.java
@@ -0,0 +1,40 @@
+4. Contiguous Array - LeetCode
+/*
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+Example 1:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1. */
+
+// One‑line memory trick : 
+// Same prefix sum seen again → subarray sum is zero.
+// Earliest time you saw it = longest route back.
+// Latest time you saw it = shorter route.
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int currentPrefixSum = 0; int length = 0; int maxLength = 0; 
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        // Key idea - transform all the 0s in the nums array to 1
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) nums[i] = -1;
+        }
+
+        // Now, the question reduces to finding the length of the longest subarray whose sum is 0
+        for(int i = 0; i < nums.length; i++) {
+            currentPrefixSum += nums[i];
+
+            if(map.containsKey(currentPrefixSum)) {
+                length = i - map.get(currentPrefixSum);
+                maxLength = Math.max(maxLength, length);
+            } else {
+                map.put(currentPrefixSum, i);
+            }
+        }
+       return maxLength; 
+    }
+} // TC : O(n)
