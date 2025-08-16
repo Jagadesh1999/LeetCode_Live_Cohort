b8e6539605ffa86221c83dbcceafc4b382660f89
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-16 13:49:30 +0530

    Create 1.4 Upperbound implementation of an element.java

---
+++ 3 Binary Search/LeetCode Problems/1.4 Upperbound implementation of an element.java
@@ -0,0 +1,40 @@
+// 4.Upperbound implementation of an element
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    private static int check(int mid, int[] nums, int target) {
+        if(nums[mid] > target) return 1;
+        else return 0;
+    }
+    
+    public static int upperbound(int[] nums, int target) {
+        int low = 0; 
+        int high = nums.length-1;
+        int res = nums.length;
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(check(mid, nums, target) == 0) {
+                low = mid + 1;
+            } else {
+                res = mid;
+                high = mid - 1;
+            }
+        }
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        int length = sc.nextInt();
+        
+        int[] nums = new int[length];
+        for(int i = 0; i < length; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(upperbound(nums, target));
+    }
+}
