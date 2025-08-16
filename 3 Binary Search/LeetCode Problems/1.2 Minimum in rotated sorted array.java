cc470a3bbc5ab960b87142a0b959339f70e02e57
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-16 12:30:37 +0530

    Update 1.2 Minimum in rotated sorted array.java

--- 3 Binary Search/LeetCode Problems/1.2 Minimum in rotated sorted array.java
+++ 3 Binary Search/LeetCode Problems/1.2 Minimum in rotated sorted array.java
@@ -2,28 +2,30 @@
 
 // Logic : The minimum point after rotation has the next point smaller than itself.
 
+// Logic : 
+// The minimum point after rotation has the next point smaller than or equal to itself and alse the 
+// before point greater than or equal to itself
+
 class Solution {
-    public boolean check(int[] nums, int mid, int high) {
-        if(nums[mid] <= nums[high]) return true;
-        else return false;
+    public boolean check(int[] nums, int mid) {
+        return (nums[mid] < nums[0]);
     }
 
     public int findMin(int[] nums) {
         int low = 0; 
         int high = nums.length - 1;
-        int res = nums[0];
+        int res = nums.length;
 
         while(low <= high) {
-            int mid = (low + high) / 2;
-            if(check(nums, mid, high)) {
-                if(nums[mid] < res) {
-                    res = nums[mid];
-                }
-                high = mid-1;
-            } else {
+            int mid = low + (high - low) / 2;
+            if(!check(nums, mid)) {
                 low = mid + 1;
+            } else {
+                res = mid;
+                high = mid - 1;
             }
         }
-        return res;
+        return (res == nums.length) ? nums[0] : nums[res];
+
     }
 } // TC : O(log2(n))
