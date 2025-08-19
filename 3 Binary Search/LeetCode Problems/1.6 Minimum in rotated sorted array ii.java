82a23a254aeb00789d96872da35451e1fa083436
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-08-19 15:45:06 +0530

    Update 1.6 Minimum in rotated sorted array ii.java

--- 3 Binary Search/LeetCode Problems/1.6 Minimum in rotated sorted array ii.java
+++ 3 Binary Search/LeetCode Problems/1.6 Minimum in rotated sorted array ii.java
@@ -1,2 +1,27 @@
 // 6. Minimum in rotated sorted array ii (The input array contains duplicates)
 
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int res = 0;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] < nums[res]) {
+                res = mid;  // Update res when a smaller value is found
+            }
+
+            if(nums[mid] == nums[high]) {
+                high--;
+            } else if(nums[mid] < nums[high]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return nums[res];
+    }
+} // TC : O(log n)
+
