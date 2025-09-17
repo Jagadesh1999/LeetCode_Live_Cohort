21c9d08610bade74188a0eb0db1f8fd152b432cc
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-17 15:22:57 +0530

    Update 2.0 Minimum Window Substring.java

--- 1 Sliding Window/2 Problems/2.0 Minimum Window Substring.java
+++ 1 Sliding Window/2 Problems/2.0 Minimum Window Substring.java
@@ -5,46 +5,51 @@
 
 class Solution {
     public String minWindow(String s, String t) {
-        // Windows Data structure
-        Map<Character, Integer> need = new HashMap();
-        for(char c : t.toCharArray()) {
-            need.put(c, need.getOrDefault(c, 0) + 1);
-        }
-        int required = t.length(); // number of characters still needed.
+        if(s.length() < t.length()) return ""; // Edge Case Handling
 
-        // Markers and the necessary variables
-        int tail = 0; int head = -1; int min_start = 0; int min_length = Integer.MAX_VALUE;
+        Map<Character, Integer> targetMap = new HashMap<>();
+        for(char c : t.toCharArray()) {
+            targetMap.put(c, targetMap.getOrDefault(c, 0) + 1);
+        }
+
+        int required = t.length();
+
+        int tail = 0;
+        int head = -1;
+        int minStart = 0;
+        int minLength = Integer.MAX_VALUE;
 
         while(tail < s.length()) {
-            while(head + 1 < s.length() && required > 0) { 
+            while(head + 1 < s.length() && required > 0) {
                 head++;
-                if(need.getOrDefault(s.charAt(head),0) > 0) {
-                    required--;
-                }
-                need.put(s.charAt(head), need.getOrDefault(s.charAt(head), 0) - 1);
+                targetMap.put(s.charAt(head), targetMap.getOrDefault(s.charAt(head), 0) - 1);
+                if(targetMap.getOrDefault(s.charAt(head), 0) >= 0) required --;
             }
 
-            if(required == 0) {
-                if(min_length > head - tail + 1) {
-                min_start = tail;
-                min_length = head - tail + 1;
-                }
+            if(required == 0 && head - tail + 1 < minLength) { // <-- Fix here
+                minLength = head - tail + 1;
+                minStart = tail;
             }
 
             if(tail > head) {
                 tail++;
                 head = tail - 1;
-                need.clear();
-                required = t.length(); // This part is very important
-            }else{
-                need.put(s.charAt(tail), need.get(s.charAt(tail)) + 1);
-                if(need.get(s.charAt(tail)) > 0) {
+                targetMap.clear();
+                for(char c : t.toCharArray()) {
+                    targetMap.put(c, targetMap.getOrDefault(c, 0) + 1);
+                }
+                required = t.length();
+            } else {
+                targetMap.put(s.charAt(tail), targetMap.getOrDefault(s.charAt(tail), 0) + 1);
+                if(targetMap.getOrDefault(s.charAt(tail), 0) > 0) {
                     required++;
                 }
                 tail++;
             }
         }
-        if(min_length == Integer.MAX_VALUE) return "";
-        else return s.substring(min_start, min_start + min_length);
+        if(minLength != Integer.MAX_VALUE) {
+            return s.substring(minStart, minStart + minLength);
+        } 
+        return "";
     }
-} // TC : O(n)
+} // TC : O(n + m)
