373841445daf375772b4e185f912559d51551f84
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 00:26:09 +0530

    Update 1 Check if a stream of num is even or odd.java

--- Introduction/3_Loops/1 Check if a stream of num is even or odd.java
+++ Introduction/3_Loops/1 Check if a stream of num is even or odd.java
@@ -1,1 +1,23 @@
+// 1 Check if a stream of numbers is odd or even
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    
+	    while(n != 0) {
+	        int number = sc.nextInt();
+	        if(number % 2 == 0) {
+	            System.out.println("The number " + number + " is even");
+	        } else {
+	            System.out.println("The number " + number + " is odd");
+	        }
+	        n--;
+	    }
+	}
+} // TC : O(n), SC : O(1)
 
