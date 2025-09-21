96cf81a37755b11c370dffc320f67d61a6808ba6
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-21 23:42:29 +0530

    Create 1 Check positive or negative.java

---
+++ Introduction/2_Conditionals/1 Check positive or negative.java
@@ -0,0 +1,12 @@
+// 1. Check if positive, negative or zero
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int num = sc.nextInt();
+	    
+	    if(num > 0) System.out.println("Positive");
+	    else if(num < 0) System.out.println("Negative");
+	    else System.out.println("Zero");
+	}
+} // TC : O(1), SC : O(1)
