7fa564ded62d338fbc025bb1cfc24a1569576e92
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 00:39:33 +0530

    Create 4 CheckPrimesTillN.java

---
+++ Introduction/3_Loops/4 CheckPrimesTillN.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class CheckPrimesTillN {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        boolean isPrime = true;
+
+        for(int i = 1; i <= num; i++) {
+            // for(int j = 2; j < num; j++){
+            //     if(num % j == 0){
+            //         isPrime = false;
+            //         break;
+            //     }
+            // }
+    
+            // 64 -> 1*64, 2*32, 4*16, 8*8, 16*4 
+            for(int j = 2; j * j <= num; j++){ // i <= root(num) = i*i <= num
+                if(num % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime == true){
+                System.out.println(i + " is a prime number.");
+            } else {
+                System.out.println(i + " is not a prime number.");
+            }
+        }
+    }
+} // TC : O(n * n), SC : O(1)
