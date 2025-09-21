7bd03b97255d7ff9352228995c052715b12853b5
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 01:03:13 +0530

    Update 1 Fibonacci Series.java

--- Introduction/4_Number System/1 Fibonacci Series.java
+++ Introduction/4_Number System/1 Fibonacci Series.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    // Function to return list containing first n fibonacci numbers.
+    public static int[] fibonacciNumbers(int n) {
+        int[] fibonacciSeries = new int[n];
+        
+        if (n >= 1) fibonacciSeries[0] = 0;
+        if (n >= 2) fibonacciSeries[1] = 1;
+    
+        int a = 0, b = 1;
+        
+        for(int i = 2; i < n; i++) {
+            int sum = a + b;
+            fibonacciSeries[i] = sum;
+            a = b;
+            b = sum;
+        }
+        
+        return fibonacciSeries;
+    }
+} // TC : O(n), SC : O(n)
