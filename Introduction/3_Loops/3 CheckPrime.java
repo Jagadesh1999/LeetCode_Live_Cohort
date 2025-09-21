f86cde223e7058e87c054487e3918551a6f22380
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 00:32:17 +0530

    Create 3 CheckPrime.java

---
+++ Introduction/3_Loops/3 CheckPrime.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Prime {
+    public static void CheckPrime(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int num = scn.nextInt();
+        boolean isPrime = true;
+
+        // for(int i = 2; i < num; i++){
+        //     if(num % i == 0){
+        //         isPrime = false;
+        //         break;
+        //     }
+        // }
+
+        // 64 -> 1*64, 2*32, 4*16, 8*8, 16*4 
+        for(int i = 2; i*i <= num; i++){ // i <= root(num) = i*i <= num
+            if(num % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime == true){
+            System.out.println("This is a prime number.");
+        } else {
+            System.out.println("This is not a prime number.");
+        }
+    }
+} // TC : O(n), SC : O(1)
