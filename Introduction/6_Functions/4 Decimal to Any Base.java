5b96691c4b8a3e67a35efd975a5ff779a39c617e
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 22:48:59 +0530

    Update 4 Decimal to Any Base.java

--- Introduction/6_Functions/4 Decimal to Any Base.java
+++ Introduction/6_Functions/4 Decimal to Any Base.java
@@ -1,1 +1,30 @@
+// Decimal to any base
+
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int number = sc.nextInt();
+		int base = sc.nextInt();
+		
+		System.out.println(decimalToBase(number, base));
+	}
+	
+	public static int decimalToBase(int number, int base) {
+		int result = 0; int place = 1;
+		
+		while(number != 0) {
+		    int remainder = number % base;
+		    result = result + (remainder * place);
+		    place *= 10;
+		    number /= base;
+		}
+		
+		return result;
+	} 
+} // TC : O(digits), SC : O(1)
 
