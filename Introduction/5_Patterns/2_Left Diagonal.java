e4ca8e4aafb78efa261640ee869ec6cb5162bdf9
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 11:21:53 +0530

    Create 2_Left Diagonal.java

---
+++ Introduction/5_Patterns/2_Left Diagonal.java
@@ -0,0 +1,25 @@
+// Left Diagonal of a matrix
+
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for(int i = 0; i < num; i++) {
+		    for(int j = 0; j < num; j++) {
+		        if(i + j == num-1) {
+		            System.out.print("* ");
+		        } else {
+		            System.out.print(" ");
+		        }
+		    }
+		    System.out.println();
+		}
+		
+	}
+} // TC : O(n * n), SC : O(1)
