1f90bb21ee01d54c9cec9bd0ecb91a2b44b3aea3
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 15:15:31 +0530

    Create 2.0 Diagonal star patterns.java

---
+++ Introduction/5_Patterns/2.0 Diagonal star patterns.java
@@ -0,0 +1,28 @@
+* * * * * 
+* *   * * 
+*   *   * 
+* *   * * 
+* * * * * 
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        for(int i = 0; i < num; i++) {
+            for(int j = 0; j < num; j++) {
+                if(i + j == num-1 || i == j || i == 0 || i == num-1 || j == 0 || j == num-1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            } 
+            System.out.println();
+        }
+    }
+} // TC : O(n * n), SC : O(1)
+
+
