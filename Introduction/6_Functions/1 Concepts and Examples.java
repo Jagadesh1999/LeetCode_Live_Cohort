06ac2c01120d0760ed092accfb23050113e08ed5
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 22:09:14 +0530

    Update and rename 1_Frequency of a Digit.java to 1 Concepts and Examples.java

---
+++ Introduction/6_Functions/1 Concepts and Examples.java
@@ -0,0 +1,22 @@
+// Do nothing and findSum 
+class Main {
+    public static void doNothing(char x, int d){
+        System.out.println("Doing nothing");
+    }
+
+    public static int findSum(int a, int b){
+        int sum = a + b;
+
+        return sum;
+    }
+
+    public static void main(String[] args){
+        int a = 55;
+        char b = 'a';
+
+        doNothing(b,a);
+
+        int sum = findSum(5,11);
+        System.out.println(sum);
+    }
+}
