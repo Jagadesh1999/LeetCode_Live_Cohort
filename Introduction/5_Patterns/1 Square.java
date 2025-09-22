62e1777919ccc9376cd13bf66ea2ec6def4caba3
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 11:25:09 +0530

    Update 1 Square.java

--- Introduction/5_Patterns/1 Square.java
+++ Introduction/5_Patterns/1 Square.java
@@ -1,3 +1,15 @@
+/*
+Framework :
+-- Identify the canvas size.
+-- Get clear idea on range of rows and columns as canvas size is already knowm.
+-- Run loops(nested) for rows and columns according to the canvas range.
+-- Use lines of equation according to the type of line. Substitute i and j values of any cell holding the value on that 
+   line. Find the value of constant(c). Now, just frame back the line equation with c value alone.
+-- Do not hard code c value. Use a dynamic value ( Logical relation to N ).   
+-- The final obtained line equation having value of c is the condition which needs to be checked before printing any 
+   cell.
+*/
+
 import java.io.*;
 import java.util.*;
 
