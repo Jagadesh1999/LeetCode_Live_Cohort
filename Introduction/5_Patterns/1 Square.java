6d02821b7112fee8af298f09b6c0cc94914495e6
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-22 11:13:35 +0530

    Update 1 Square.java

--- Introduction/5_Patterns/1 Square.java
+++ Introduction/5_Patterns/1 Square.java
@@ -12,7 +12,7 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int num = sc.nextInt();
 		
