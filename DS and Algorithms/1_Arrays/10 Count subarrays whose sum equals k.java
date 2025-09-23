03c13c7950aec2a9c055d38be39b6133489e26ea
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-23 23:47:29 +0530

    Create 10 Count subarrays whose sum equals k.java

---
+++ DS and Algorithms/1_Arrays/10 Count subarrays whose sum equals k.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void solve(int[] nums, int k) { 
+	    int count = 0; 
+	    
+	    for(int si = 0; si < nums.length; si++) { // To represent each eligible start index
+	        for(int ei = si; ei < nums.length; ei++) { // To represent a group of end indices for each start index
+	            int sum = 0;
+	            for(int idx = si; idx <= ei; idx++) { // To print a subarray in the range of si-ei (both inclusive)
+	                sum += nums[idx];
+	            }
+	            if(sum == k) count++;
+	        }
+	    }
+	    
+	    System.out.println(count);
+	} // TC : O(n^3), SC : O(1)
+	
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int size = sc.nextInt();
+	    
+	    int[] nums = new int[size];
+	    for(int i = 0; i < size; i++) {
+	        nums[i] = sc.nextInt();
+	    }
+	    
+	    int k = sc.nextInt();
+	    
+	    solve(nums, k);
+	}
+} 
