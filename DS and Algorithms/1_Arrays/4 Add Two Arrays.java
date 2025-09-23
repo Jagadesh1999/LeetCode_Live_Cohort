ba4928f382a7ee97b8dfe0682333aa533a8c5bc2
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-23 17:18:47 +0530

    Create 4 Add Two Arrays.java

---
+++ DS and Algorithms/1_Arrays/4 Add Two Arrays.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void solve(int[] nums_1, int[] nums_2) {
+	    List <Integer> resultList = new ArrayList<>();
+	    
+	    int i = nums_1.length - 1;
+	    int j = nums_2.length - 1;
+	    int carry = 0;
+	    
+	    while(i >= 0 || j >= 0 || carry > 0) {
+	        int sum = carry;
+	        
+	        if (i >= 0) {
+                sum += nums_1[i];
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += nums_2[j];
+                j--;
+            }
+            
+            // The new digit is sum % 10;
+            resultList.add(sum % 10);
+            
+            // The quotient is the new carry
+            carry = sum / 10;
+	    }
+	    
+	    Collections.reverse(resultList); 
+	    
+	    // Convert the List to an int array and return
+        int[] resultNums = new int[resultList.size()];
+        for (int k = 0; k < resultList.size(); k++) {
+            resultNums[k] = resultList.get(k);
+        }
+        
+        // Convert digits to single integer
+        // long result = 0;
+        // for (int digit : resultNums) {
+        //     result = result * 10 + digit;
+        // }
+
+        for(int l = 0; l < resultNums.length; l++) {
+	       System.out.print(resultNums[l] + " ");
+	    }
+        
+	} // TC : O(n), SC : O(1)
+	
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int size_1 = sc.nextInt();
+	    
+	    int[] nums_1 = new int[size_1];
+	    for(int i = 0; i < size_1; i++) {
+	        nums_1[i] = sc.nextInt();
+	    }
+	    
+	    int size_2 = sc.nextInt();
+	    
+	    int[] nums_2 = new int[size_2];
+	    for(int i = 0; i < size_2; i++) {
+	        nums_2[i] = sc.nextInt();
+	    }
+	    
+	    solve(nums_1, nums_2);
+	}
+} 
