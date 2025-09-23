13e605a307eee357115ff624557a231966e6ebda
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-23 23:14:06 +0530

    Update 7 Reverse the array.java

--- DS and Algorithms/1_Arrays/7 Reverse the array.java
+++ DS and Algorithms/1_Arrays/7 Reverse the array.java
@@ -19,7 +19,7 @@ public class Main
 	    for(int i = 0; i < nums.length; i++) {
 	        System.out.print(nums[i] + " ");
 	    }
-	} // TC : O(log n), SC : O(1)
+	} // TC : O(n), SC : O(1)
 	
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
