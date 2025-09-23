095f373795b53e113906edaa7d58f48d1f64dd79
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-23 22:58:00 +0530

    Create 7 Reverse the array.java

---
+++ DS and Algorithms/1_Arrays/7 Reverse the array.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void solve(int[] nums) {
+	    int start = 0;
+	    int end = nums.length-1;
+	    
+	    while(start < end) {
+	        int temp = nums[start];
+	        nums[start] = nums[end];
+	        nums[end] = temp;
+	        
+	        start++;
+	        end--;
+	    }
+	    
+	    for(int i = 0; i < nums.length; i++) {
+	        System.out.print(nums[i] + " ");
+	    }
+	} // TC : O(log n), SC : O(1)
+	
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int size = sc.nextInt();
+	    
+	    int[] nums = new int[size];
+	    for(int i = 0; i < size; i++) {
+	        nums[i] = sc.nextInt();
+	    }
+	    
+	    solve(nums);
+	}
+} 
