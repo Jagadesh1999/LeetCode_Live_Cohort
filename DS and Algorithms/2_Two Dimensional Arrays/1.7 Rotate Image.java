8c0c0eb365e7d310cf4adc5c695377a2b26141f2
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-26 16:03:30 +0530

    Create 1.7 Rotate Image.java

---
+++ DS and Algorithms/2_Two Dimensional Arrays/1.7 Rotate Image.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int N = matrix.length;
+
+        // 1. Transpose the matrix
+        // We iterate only over the upper triangle (j >= i) to avoid double-swapping.
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // 2. Reverse each row
+        // This is done using a two-pointer approach (left and right).
+        for (int i = 0; i < N; i++) {
+            int left = 0;
+            int right = N - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+} // TC : O(n^2)
