fd819b41925e2c0d5e0e16028ac9de129f20eeb9
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-28 18:54:35 +0530

    Update 1.8 Non standard diagonal traversal.java

--- DS and Algorithms/2_Two Dimensional Arrays/1.8 Non standard diagonal traversal.java
+++ DS and Algorithms/2_Two Dimensional Arrays/1.8 Non standard diagonal traversal.java
@@ -4,7 +4,7 @@ Observation - (The starting element of each row and the ending element of each c
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         for(int k = 0; k < mat.length; k++) {
-            int i = k;
+            int i = k; // Know the reasoning behind the initialisation
             int j = 0;
 
             while(i >= 0 && j < mat[0].length) { // Know the reason behind the while loop condition
