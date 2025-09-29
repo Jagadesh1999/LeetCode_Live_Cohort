2f6ca360254885a4c2f3fe19ce4ad8e42ffa360a
Jagadesh1999 <88584143+Jagadesh1999@users.noreply.github.com>
2025-09-29 13:12:44 +0530

    Create 1.5 Finding the Factorial - Recursion.java

---
+++ DS and Algorithms/3_Recursion/1.5 Finding the Factorial - Recursion.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    // 1: Define the recursive function with the correct input parameters.
+    public static int factorial(int number) {
+        // 2: Base case: when to stop
+        if(number == 0) {
+            return 1; // Exit the recursion here
+        }
+        
+        // 3: Recursive case :
+        return number * factorial(number - 1); // Make a choice and calling the function
+    }
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		
+		System.out.print(factorial(number));
+	}
+} // TC : O(n), SC : O(n)
